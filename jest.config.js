3e064bf2a5d2213872c73ff9076031de15e5511c
Bartlack, Mario <mario.bartlack@spring-media.de>
2020-03-28 22:42:43 +0100

    - do not collect coverage from definition files

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ['**/*.{js,ts,vue}', '!**/*.stories.js'],
+  collectCoverageFrom: ['**/*.{js,ts,vue}', '!**/*.stories.js', '!**/*.d.ts'],
 
   // A list of reporter names that Jest uses when writing coverage reports
   coverageReporters: ['text', 'text-summary'],

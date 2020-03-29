de47400834a386212209a22ef18c192c92bd3949
Bartlack, Mario <mario.bartlack@spring-media.de>
2020-03-29 20:06:02 +0200

    - add (experimental) function to bypass the internal cache in getCustomVendorConsents in Sourcepoint library

--- src/tcf-v2/get-custom-vendor-consents.ts
+++ src/tcf-v2/get-custom-vendor-consents.ts
@@ -1,5 +1,53 @@
 import { executeMethod } from './tcf';
 import { CustomVendorConsentsResult } from '../types';
+import { getClientLibraryUrl, getIABStubScript } from './configurations';
 
 export const getCustomVendorConsents = (): Promise<CustomVendorConsentsResult> =>
   executeMethod('getCustomVendorConsents');
+
+export const getCustomVendorConsentsBypassCache = (): Promise<CustomVendorConsentsResult> => {
+  // Check browser support for srcdoc attribute (i'm talking to you IE)
+  if (!Boolean('srcdoc' in document.createElement('iframe'))) {
+    return getCustomVendorConsents();
+  }
+
+  if (!window?._sp_?.config) {
+    return getCustomVendorConsents();
+  }
+
+  const { accountId, wrapperAPIOrigin, propertyId, mmsDomain } = window._sp_.config;
+
+  return new Promise(resolve => {
+    const iframe = document.createElement('iframe');
+    iframe.srcdoc = `
+      <script>${getIABStubScript()}</script>
+      <script>
+        window._sp_ = {
+          config: {
+            accountId: ${accountId},
+            wrapperAPIOrigin: ${wrapperAPIOrigin},
+            propertyId: ${propertyId},
+            mmsDomain: ${mmsDomain},
+          },
+        };
+      </script>
+      <script src="${getClientLibraryUrl()}"></script>
+      <script>
+        __tcfapi('getCustomVendorConsents', null, function(consents) {
+          window.parent.postMessage(consents, '*');
+        });
+      </script>
+    `;
+
+    const onMessage = (event: MessageEvent): void => {
+      window.removeEventListener('message', onMessage);
+      document.body.removeChild(iframe);
+
+      return resolve(event.data);
+    };
+
+    window.addEventListener('message', onMessage, false);
+
+    document.body.appendChild(iframe);
+  });
+};

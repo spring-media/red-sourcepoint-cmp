1ea83a743abb551c8349be9c310096932c59d6af
Bartlack, Mario <mario.bartlack@spring-media.de>
2020-04-22 15:10:20 +0200

    - extends tests for cae files

--- tests/cae-files.test.js
+++ tests/cae-files.test.js
@@ -1,23 +1,31 @@
 const { existsSync, readdirSync, readFileSync } = require('fs');
 const { parse, resolve } = require('path');
 
+const collectFilesFromConfig = () => {
+  const caeDir = '.cae';
+  const { version } = require('../package.json');
+  const config = require(`../.cae/red-cmp-config-${version}.json`);
+
+  return Object.values(config).map((file) => [file, resolve(caeDir, file)]);
+};
+
 const prepareTests = () => {
   const caeDir = '.cae';
 
-  if (!existsSync(caeDir)) {
-    return [];
-  }
-
   return readdirSync(caeDir)
     .map((file) => parse(file))
-    .filter(({ ext }) => ['.css', '.html', '.json'].includes(ext))
+    .filter(({ ext }) => ['.css', '.html'].includes(ext))
     .map(({ base }) => ({ base, path: resolve(caeDir, base) }))
     .map((entry) => ({ name: entry.base, content: readFileSync(entry.path, 'utf8') }))
-    .map((entry) => [entry.name, entry.content]);
+    .map((entry) => [entry.name.replace(/-\d+\.\d+\.\d+(\.min)?/, ''), entry.content]);
 };
 
 describe('Testsuite CAE files', () => {
-  it.each([...prepareTests()])('%s should have the expected content', (name, content) => {
+  it.each(collectFilesFromConfig())('file %s should exists', (name, path) => {
+    expect(existsSync(path)).toBe(true);
+  });
+
+  it.each(prepareTests())('%s should have the expected content', (name, content) => {
     expect(content).toMatchSnapshot();
   });
 });

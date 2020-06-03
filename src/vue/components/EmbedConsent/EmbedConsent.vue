3e41d4043b57dbc95cec0478f78c38379ec14ba6
Bartlack, Mario <mario.bartlack@spring-media.de>
2020-06-03 18:09:50 +0200

    feat(vue): adds a generic embed-consent component for (social) vendors who doesn't have a dedicated component

---
+++ src/vue/components/EmbedConsent/EmbedConsent.vue
@@ -0,0 +1,41 @@
+<template>
+  <consent-wrapper :vendorId="vendorId">
+    <template #disabledContent>
+      <embed-placeholder :privacyManagerId="privacyManagerId"></embed-placeholder>
+    </template>
+    <template #enabledContent>
+      <div v-html="content"></div>
+    </template>
+  </consent-wrapper>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { ConsentWrapper } from '../ConsentWrapper';
+import { EmbedPlaceholder } from '../EmbedPlaceholder';
+
+type Props = {
+  vendorId: string;
+  content: string;
+  privacyManagerId: number;
+};
+
+export default Vue.extend<{}, {}, {}, Props>({
+  name: 'EmbedInstagramConsent',
+  components: { EmbedPlaceholder, ConsentWrapper },
+  props: {
+    privacyManagerId: {
+      type: Number,
+      required: true,
+    },
+    content: {
+      type: String,
+      default: '',
+    },
+    vendorId: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>

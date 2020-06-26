8103004f909031d68c9f7f5d54b906feda919ff5
Bartlack, Mario <mario.bartlack@spring-media.de>
2020-06-26 21:19:33 +0200

    chore(vue): update storybook to version 6 (beta)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,20 +1,4 @@
 module.exports = {
-  stories: ['../src/vue/**/*.stories.(mdx|js)'],
+  stories: ['../src/vue/**/*.stories.@(mdx|js)'],
   addons: ['@storybook/addon-docs', '@storybook/addon-viewport', '@storybook/addon-knobs'],
-  webpackFinal: async config => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      use: [{
-        loader: 'ts-loader',
-        options: {
-          appendTsSuffixTo: [/\.vue$/],
-          transpileOnly: true
-        },
-      }],
-    });
-
-    config.resolve.extensions.push('.ts', '.tsx');
-
-    return config;
-  },
 };
